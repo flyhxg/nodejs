ea367d66df0df37d81af16552b16ce2eaae90234
wangjiang <wangjiang112357@gmail.com>
2023-06-01 00:41:01 +0800

    项目2初始化

--- utils/images.ts
+++ utils/images.ts
@@ -1,26 +1,40 @@
 
 const COMMON = {
-  LOGO_PNG: '/images/common/logo.png'
+  LOADING_SVG: '/images/common/loading.svg',
+  LOGO_PNG: '/images/common/logo.png',
+  LOGO_BLACK_SVG: '/images/common/logo_black.svg',
+  LOGO_WHITE_SVG: '/images/common/logo_white.svg',
+  STATUS_LOADING_SVG: '/images/common/status_loading.svg',
+  STATUS_SUCESS_SVG: '/images/common/status_sucess.svg',
+  STATUS_WARN_SVG: '/images/common/status_warn.svg'
+}
+      
+
+const DIALOG = {
+  CLOSE_SVG: '/images/dialog/close.svg',
+  STATUS_LOADING_SVG: '/images/dialog/status_loading.svg',
+  STATUS_SUCESS_SVG: '/images/dialog/status_sucess.svg',
+  STATUS_WARN_SVG: '/images/dialog/status_warn.svg'
 }
       
 
 const HOME = {
-  BTC1_PNG: '/images/home/btc1.png',
-  BTC2_PNG: '/images/home/btc2.png',
-  BTC3_PNG: '/images/home/btc3.png',
-  COMING_PNG: '/images/home/coming.png',
-  COMING_TEXT_PNG: '/images/home/coming_text.png',
+  BTC_PNG: '/images/home/btc.png',
+  COOMING_SOON_SVG: '/images/home/cooming_soon.svg',
+  COVER_PNG: '/images/home/cover.png',
   PERSON_PNG: '/images/home/person.png'
 }
       
 
 const SOCIALS = {
-  DISCORD_PNG: '/images/socials/discord.png',
-  TWITTER_PNG: '/images/socials/twitter.png'
+  DISCORD_SVG: '/images/socials/discord.svg',
+  TELEGRAM_SVG: '/images/socials/telegram.svg',
+  TWITTER_SVG: '/images/socials/twitter.svg'
 }
       
 export const Images = {
   COMMON,
+  DIALOG,
   HOME,
   SOCIALS
 }       
