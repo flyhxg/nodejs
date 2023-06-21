ab8bf066b04311b8a232843058f39f895b2a2f17
wangjiang <wangjiang112357@gmail.com>
2023-06-21 14:48:03 +0700

    [GoogleAnalyze]:接入谷歌分析

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import WalletContextProvider from './context/WalletContext'
 import { Metadata } from 'next'
 import Script from 'next/script'
 import NProgressBar from './views/common/NProgressBar'
+import GoogleAnalyze from './views/common/GoogleAnalyze'
 
 export const metadata: Metadata = {
   title: 'Banana Market',
@@ -18,18 +19,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={pixelFont.className}>
-      {/*<head>*/}
-      {/*  <script async src="https://www.googletagmanager.com/gtag/js?id=G-XKJRE222G9"></script>*/}
-      {/*  <script>*/}
-      {/*    window.dataLayer = window.dataLayer || [];*/}
-      {/*    function gtag(){dataLayer.push(arguments);}*/}
-      {/*    gtag('js', new Date());*/}
-
-      {/*    gtag('config', 'G-XKJRE222G9');*/}
-      {/*  </script>*/}
-      {/*</head>*/}
       <head>
         <Script src={'/js/mempool.js'} />
+        <GoogleAnalyze id={'G-XKJRE222G9'} />
       </head>
       <body>
         <StyledComponentsRegistry>
---
+++ app/views/common/GoogleAnalyze.tsx
@@ -0,0 +1,16 @@
+import Script from 'next/script'
+
+export default function GoogleAnalyze(props: { id: string }) {
+  return (
+    <>
+      <Script async src={`https://www.googletagmanager.com/gtag/js?id=${props.id}`}></Script>
+      <Script id={'google-analytics'} strategy={'afterInteractive'}>
+        {`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag("js", new Date());
+
+            gtag("config", "${props.id}");`}
+      </Script>
+    </>
+  )
+}
